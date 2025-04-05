f5d64932a716e5a552075d605f2a0d27dc546e24
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-05 08:18:49 +0000

    container configure done

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("hitesh");
+let accountemail="amshjk@123";
+var accountpassword="12345678";
+ accountcity="bengalore";
+accountEmail="ansh@1234";
+accountcity="gpj";
+
+console.table([ accountEmail, accountpassword, accountcity])
