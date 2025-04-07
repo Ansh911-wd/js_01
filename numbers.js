2eda6591e073b7697ed4c0f46c6f578a97703088
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-07 17:59:37 +0000

    done

---
+++ numbers.js
@@ -0,0 +1,15 @@
+const score= 400
+ const balance = new Number(100)
+  console.log(balance);
+console.log(balance.toString ().length);
+
+const otherNumber= 99.889
+console.log(otherNumber.toPrecision(4));
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math. round(4.6));
+console.log(Math. ceil(4.6));
+console.log(Math. floor(4.6));
+
+console.log(Math. sqrt(4.6));
+console.log(Math. random(4.6));
