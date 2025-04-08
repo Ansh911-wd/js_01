e7aba4e55268feb0df6127e3e908bc5e98089385
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-08 17:18:57 +0000

    js func

--- obj.js
+++ obj.js
@@ -1,6 +1,42 @@
-const tinderUser ={}
-const tinderUser.id="123abc"
-const tinderUser.name= "ansh"
-tinderUser.isLoggedin=false
+// // const tinderUser ={}
+// const tinderUser.id="123abc"
+// const tinderUser.name= "ansh"
+// tinderUser.isLoggedin=false
 
 
+function nice (name){
+    console.log("hey " + name + " be calm u r going to achieve grand ")
+    console.log("hey " + name + " be calm u r going to be placed for more than you expect")
+    console.log("hey " + name + " be calm u r going to achieve everything that u have think of")
+    console.log("hey " + name + " have patience")
+    console.log("hey " + name + " be polite")
+    console.log("hey " + name + " be normal")
+}
+
+nice("me")
+
+console.log("hey Ansh be calm u r going to achieve grand")
+console.log("hey Ansh be calm u r going to be placed for more than you expect")
+console.log("hey Ansh be calm u r going to achieve everything that u have think of")
+console.log("hey Ansh be polite")
+console.log("hey Ansh have patience")
+console.log("hey Ansh be normal")
+function sum(a, b){
+    console.log(a + b)
+} sum(8,3)
+
+
+function sum(a, b, c = 5) {
+    console.log(a + b)
+    return a + b + c
+}
+result1 = sum(18, 3)
+result2 = sum(8, 18)
+result3 = sum(5, 6)
+result4 = sum(5, 7)
+console.log("The sum of these numbers is", result1)
+console.log("The sum of these numbers is", result2)
+console.log("The sum of these numbers is", result3)
+  console.log("sum of the numbers is", result4)
+  console.log("sum of the numbers is", result4)
+  console.log("sum of the numbers is", result4)
