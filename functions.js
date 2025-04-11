d85bfe6e9efdcb1a8ddd7fb33b7754d96df939c0
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-11 18:20:49 +0000

    01_fn done

--- functions.js
+++ functions.js
@@ -37,3 +37,29 @@ const result= addTwoNumbers(3,5)
 
 console.log( "Result: ",result); // undefined result aayegii
 
+function loginUserMessage(username)
+{
+    if (username===undefined)
+         { console.log("please enter a username");
+        
+    }
+    return `${username} just logged in`
+}
+//  console.log(loginUserMessage("Ansh"))
+// console.log(loginUserMessage(""))
+console.log(loginUserMessage())
+
+function loginUserMessage(username)
+{
+    if (username===undefined)
+         { console.log("please enter a username");
+            return// iske baad code aage i jayegii
+
+        
+    }
+    return `${username} just logged in` //yaha pe ye line execute ni hogii
+
+}
+//  console.log(loginUserMessage("Ansh"))
+// console.log(loginUserMessage(""))
+console.log(loginUserMessage())
