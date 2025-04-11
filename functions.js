3ceb68755a04820bf2a1fb43d105a1a92bba0dfc
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-11 17:58:43 +0000

    will add

---
+++ functions.js
@@ -0,0 +1,39 @@
+function sayMyName(){
+    console.log("a")
+    console.log("n")
+    console.log("s")
+    console.log("H")   
+}
+sayMyName()
+
+function addTwoNumbers( number1 ,number2)
+{
+  console.log( number1+number2)
+}
+addTwoNumbers(3,4)
+
+function addTwoNumbers( number1 ,number2)
+{
+  console.log( number1+number2)
+}
+addTwoNumbers(3,"4")
+function addTwoNumbers( number1 ,number2)
+{
+  console.log( number1+number2)
+}
+addTwoNumbers(3,"A")
+
+function addTwoNumbers( number1 ,number2)
+{
+  console.log( number1+number2)
+}
+addTwoNumbers(3,null)
+
+function addTwoNumbers( number1 ,number2)
+{
+  console.log( number1+number2);
+}
+const result= addTwoNumbers(3,5)
+
+console.log( "Result: ",result); // undefined result aayegii
+
