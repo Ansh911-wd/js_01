c06b330bdb05b691fdfe0bb941cbfad1efde9b04
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-14 07:25:43 +0000

    updation done

--- minihoist.js
+++ minihoist.js
@@ -1,11 +1,32 @@
-function one(){
-    const username = "Ansh"
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-        console.log(website);
-         two()
+// function one(){
+//     const username = "Ansh"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//         console.log(website);
+//          two()
 
-} 
-one()
+// } 
+// // one()
+
+// if(true) {
+//     const username= "Hitesh"
+//     if(username==="hitesh") {
+//         const website ="youtube"
+//         console.log(username+website);
+//     }
+//     // console.log(website)
+// }
+// console.log(username);
+
+ console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+// addone(5)
+
+const addTwo= function (num){
+    return num + 2
+}
+addTwo(5)
