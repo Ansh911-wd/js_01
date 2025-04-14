ff66906d5fa0efaf469affd36278a25d997f6ac0
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-14 07:38:12 +0000

    .. arrow

---
+++ arrow.js
@@ -0,0 +1,19 @@
+const user = {
+    username: "Ansh",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username},welcome to website`);
+        console.log(this)
+    }
+}
+user.welcomeMessage()
+user.username="sam"
+user.welcomeMessage()
+
+function chai()
+{
+    console.log(this);
+
+}
+chai()
