576a26492dad7dab92665a3a3b7613b06158bccd
Ansh911-wd <anshkrsingh321@gmail.com>
2025-04-18 10:49:19 +0000

    started

---
+++ Dom/Dom1.js
@@ -0,0 +1,20 @@
+// Select the body element
+const body = document.body;
+
+// Create a new div element
+const newDiv = document.createElement('div');
+
+// Add some text content to the div
+newDiv.textContent = 'Hello, DOM!';
+
+// Add a class to the div
+newDiv.classList.add('my-div');
+
+// Append the div to the body
+body.appendChild(newDiv);
+
+// Add some styles to the div
+newDiv.style.backgroundColor = 'lightblue';
+newDiv.style.padding = '10px';
+newDiv.style.borderRadius = '5px';
+newDiv.style.textAlign = 'center';
